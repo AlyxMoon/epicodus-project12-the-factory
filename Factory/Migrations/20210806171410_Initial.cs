30b05a83af9c840f8b99e463ebff0093fead6085
AlyxMoon <allisterkmoon@gmail.com>
2021-08-06 10:31:29 -0700

    add initial migration

---
+++ Factory/Migrations/20210806171410_Initial.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "engineers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Product = table.Column<int>(type: "int", nullable: false),
+                    CycleTime = table.Column<int>(type: "int", nullable: false),
+                    ItemsPerCycle = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_engineers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "machines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Product = table.Column<int>(type: "int", nullable: false),
+                    CycleTime = table.Column<int>(type: "int", nullable: false),
+                    ItemsPerCycle = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_machines", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EngineerMachine",
+                columns: table => new
+                {
+                    EngineersId = table.Column<int>(type: "int", nullable: false),
+                    MachinesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EngineerMachine", x => new { x.EngineersId, x.MachinesId });
+                    table.ForeignKey(
+                        name: "FK_EngineerMachine_engineers_EngineersId",
+                        column: x => x.EngineersId,
+                        principalTable: "engineers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EngineerMachine_machines_MachinesId",
+                        column: x => x.MachinesId,
+                        principalTable: "machines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EngineerMachine_MachinesId",
+                table: "EngineerMachine",
+                column: "MachinesId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EngineerMachine");
+
+            migrationBuilder.DropTable(
+                name: "engineers");
+
+            migrationBuilder.DropTable(
+                name: "machines");
+        }
+    }
+}
