0fb83f64bd2790a8996070421cc837b3a63f559b
AlyxMoon <allisterkmoon@gmail.com>
2021-08-08 15:13:07 -0700

    show list of engineers and machines in homepage

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,12 +1,34 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Factory.Models;
+using Factory.ViewModels;
 
 namespace Factory.Controllers
 {
   [Route("/")]
   public class HomeController : Controller
   {
+    private readonly DatabaseContext _db;
+
+    public HomeController(DatabaseContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet]
-    public ActionResult Index () { return View(); }
-    
+    public ActionResult Index () 
+    {
+      AllEngineersAndMachines data = new () {
+        Engineers = _db.Engineers
+          .Include(engineer => engineer.Machines)
+          .ToList(),
+        Machines = _db.Machines
+          .Include(machine => machine.Engineers)
+          .ToList()
+      };
+
+      return View(data);
+    }
   }
 }
---
+++ Factory/ViewModels/AllEngineersAndMachines.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Factory.Models;
+
+namespace Factory.ViewModels
+{
+  public class AllEngineersAndMachines
+  {
+    public List<Engineer> Engineers { get; set; }
+    public List<Machine> Machines { get; set; }
+  }
+}
